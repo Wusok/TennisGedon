651ca0c362a74756f73234b02e6ed158fc8403dc
Nahue1479 <nahuel.gutierrez@davinci.edu.ar>
2021-06-13 21:23:33 -0300

    Rugbier

---
+++ Assets/Scripts/AttackBoxRugbier.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBoxRugbier : MonoBehaviour
+{
+    [SerializeField]
+    public Animator animator;
+    [SerializeField]
+    private string boolParameterName = "TargetInRange";
+    public List<Transform> targetsInRange;
+    private Transform      closestTarget;
+    [SerializeField]
+    private GameObject projectilePreafab;
+    [SerializeField]
+    private Transform shootingPoint;
+
+
+    private void Awake()
+    {
+        targetsInRange = new List<Transform>();
+    }
+
+
+    void Update()
+    {
+        if (targetsInRange.Count > 0)
+        {
+            closestTarget = targetsInRange[0];
+            float distanceToCurrentTarget = Vector3.Distance(transform.position, closestTarget.position);
+            float distanceToNewTarget;
+            for (int i = 0; i < targetsInRange.Count; i++)
+            {
+                distanceToNewTarget = Vector3.Distance(transform.position, targetsInRange[i].position);
+                if (distanceToCurrentTarget > distanceToNewTarget)
+                {
+                    closestTarget = targetsInRange[i];
+                    distanceToCurrentTarget = distanceToNewTarget;
+                }
+            }
+
+            Vector3 targetPosition = closestTarget.position;
+            targetPosition.y = transform.root.position.y;
+
+            transform.root.LookAt(targetPosition);
+        }
+    }
+
+    public void AttackClosestTarget()
+    {
+        if (targetsInRange.Count > 0)
+        {
+            GameObject createdProjectile = Instantiate(projectilePreafab, shootingPoint.position, shootingPoint.rotation);
+            createdProjectile.transform.LookAt(closestTarget);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        targetsInRange.Add(other.transform);
+        if (targetsInRange.Count == 1)
+        {
+            animator.SetBool(boolParameterName, true);
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        targetsInRange.Remove(other.transform);
+        if (targetsInRange.Count < 1)
+        {
+            animator.SetBool(boolParameterName, false);
+        }
+    }
+
+    private void Dead()
+    {
+
+    }
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Rugbier.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rugbier : MonoBehaviour
+{
+    
+    
+    public float speed;
+    public GameObject player;
+    public AttackBoxRugbier behaviorRefetence;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (behaviorRefetence.targetsInRange.Count > 0)
+        {
+            var direction = player.transform.position - transform.position;
+            transform.position += direction * speed * Time.deltaTime;
+            behaviorRefetence.animator.SetBool("Running", true);
+        }
+        else
+        {
+            behaviorRefetence.animator.SetBool("Running", false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        float backup = speed;
+        if (other.gameObject.tag == "PunchZone")
+        {
+            behaviorRefetence.animator.SetTrigger("Attack");
+            speed = 0;
+        }
+        else
+        {
+            behaviorRefetence.animator.SetBool("Attack", false);
+            speed = backup;
+        }
+        speed = backup;
+
+
+      
+     
+    }
+
+}
