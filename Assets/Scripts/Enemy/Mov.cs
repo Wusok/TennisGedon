625827fd7ac62b4ef0183dbf91734d465962c2e0
MaiineV <agustinmartinezbarrios2@hotmail.com>
2021-06-29 15:28:23 -0300

    ia + edificios

--- Assets/Scripts/Enemy/Mov.cs
+++ Assets/Scripts/Enemy/Mov.cs
@@ -6,9 +6,15 @@ public class Mov : MonoBehaviour
 {
     GameObject player;
     int life = 10;
+    float damp = 3;
+    Rigidbody rb;
+    int speed = 10;
+    float range = 3f;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Awake()
@@ -19,14 +25,33 @@ public class Mov : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(player.transform.position, transform.position) < 30)
+        {
+            var rotate = Quaternion.LookRotation(player.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp((transform.rotation), rotate, damp * Time.deltaTime);
+
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        RayCast();
+    
+    }
+
+    void RayCast()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * range, new Color(1, 0, 0));
+        if(Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            Debug.Log(hit.collider.tag);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Pelota")
+        if (other.gameObject.tag == "Pelota")
         {
-            //other.GetComponent<BulletBeheivor>(WhatIsThisBall);
+            other.GetComponent<BulletBeheivor>();
         }
     }
 }
