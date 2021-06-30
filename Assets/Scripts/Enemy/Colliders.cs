3b2a3e80de50f6b852c1de690ac1b4553a9498ad
MaiineV <agustinmartinezbarrios2@hotmail.com>
2021-06-30 04:14:32 -0300

    asd

---
+++ Assets/Scripts/Enemy/Colliders.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colliders : MonoBehaviour
+{
+    public bool triggerOn = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            triggerOn = true;
+        }
+    }
+}
